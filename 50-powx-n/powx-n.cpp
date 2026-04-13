776338f1f35204e4a616a6020c3351f5d0a252f5
AshishKumar1278-debug <12ashishkumar78@gmail.com>
2026-04-13 18:18:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (43.74%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0)
+            return 1.0;
+        if (n < 0) {
+            return (1.0 / x) * myPow(1.0 / x, -(n + 1));
+        }
+        double half = myPow(x, n/2);
+        if(n%2 == 0){
+            return half*half;
+        }
+        else{
+            return half*half*x;
+        }
+    }
+};
